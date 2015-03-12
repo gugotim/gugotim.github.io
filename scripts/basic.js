0f030b955b516bc8975befbd721d006a951b25e6
gugotim <v.cater@qq.com>
2015-03-12 13:22:13 +0800

    correct

--- scripts/basic.js
+++
@@ -1,5 +0,0 @@
-var header = document.querySelector('#wrapper header');
-var content = document.getElementById('content');
-alert(window.getComputedStyle(header, 'height'));
-alert(window.innerHeight);
-// content.style.height = (document.body.clientHeight - header.style.height) + 'px';
