30de95818354d44dcb02dec9312e322bc487afbb
gugotim <v.cater@qq.com>
2015-03-15 10:54:26 +0800

    x

--- Xulei/scripts/basic.js
+++ Xulei/scripts/basic.js
@@ -15,6 +15,7 @@ function getBlog() {
   };   
   // xhr.open('get', 'blogs/2015-3-15', true);
   xhr.open('get', 'https://github.com/gugotim/summary/Http', true);
+  xhr.setRequestHeader('Access-Control-Allow-Origin', 'http://gugotim.github.io');
   xhr.send();
 }
 function handleNavigator() {
