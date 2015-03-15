4d8fa43755a7ed20e62f609f491dae2bba4b8eb0
gugotim <v.cater@qq.com>
2015-03-15 09:19:05 +0800

    x

--- Xulei/scripts/basic.js
+++ Xulei/scripts/basic.js
@@ -13,9 +13,7 @@ function getBlog() {
       }
     }
   };   
-  // xhr.open('get', 'blogs/2015-3-15', true);
-  xhr.open('get','https://api.github.com/repos/gugotim/summary/Http', true);
-  // xhr.setRequestHeader('Access-Control-Allow-Origin','http://gugotim.github.io/Xulei');
+  xhr.open('get', 'blogs/2015-3-15', true);
   xhr.send();
 }
 function handleNavigator() {
